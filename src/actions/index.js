0b2a48fb74aa4b4e79bbe6585bbe5ff7c5d6a7b8
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-08-13 12:16:41 -0400

    finished index js exports / finished addform.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -33,6 +33,13 @@ export const fetchFail = (error) => {
   return { type: FETCH_FAIL, payload: error };
 };
 
+export const addSmurfs = (smurfs) => {
+  return { type: ADD_SMURFS, payload: smurfs };
+};
+
+export const errorMessage = (error) => {
+  return { type: ERROR, payload: error };
+};
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
