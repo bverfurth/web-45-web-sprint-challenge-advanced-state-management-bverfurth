9c29b73bd1a4e2fb4b826a552dbeea32f5ee95cb
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-08-13 12:54:31 -0400

    installed redux-logger / working on errors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
 import "./index.css";
 import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
-const { worker } = require('./mocks/browser');
+// Middleware Section
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+
+// Reducer Section
+import reducer from "./reducers";
+
+//Store Section
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+const { worker } = require("./mocks/browser");
 worker.start();
 
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <App />, 
-    rootElement
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
 );
 
 //Task List:
