ed2b7e5bcdd44b07db5a5f0e2a10bbc62bd4e8a8
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-08-13 12:10:45 -0400

    set up app.js and finishing exports for index

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
-
-import AddForm from './components/AddForm';
-import SmurfList from './components/SmurfList';
-import Header from './components/Header';
-
-import axios from 'axios';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
+import axios from "axios";
+import { connect } from "react-redux";
+import { fetchSmurfs } from "./actions";
+import Header from "./components/Header";
+import React, { Component } from "react";
+import AddForm from "./components/AddForm";
+import "bootstrap/dist/css/bootstrap.min.css";
+import SmurfList from "./components/SmurfList";
 
 class App extends Component {
   componentDidMount() {
-    axios.get('http://localhost:3333/smurfs')
-    .then(res => console.log(res))
-    .catch(err => console.log('Axios Error', err));
+    console.log("ComponentMounted");
+    this.props.dispatch(fetchSmurfs());
   }
 
   render() {
@@ -22,15 +20,15 @@ class App extends Component {
         <Header />
 
         <main>
-          <SmurfList/>
-          <AddForm/>
+          <SmurfList />
+          <AddForm />
         </main>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(null)(App);
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,37 @@
-import axios from 'axios';
+import axios from "axios";
+
+export const FETCH_START = "FETCH_START";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_FAIL = "FETCH_FAIL";
+export const ADD_SMURFS = "ADD_SMURFS";
+export const ERROR = "ERROR";
+
+export const fetchSmurfs = () => {
+  return (dispatch) => {
+    dispatch(fetchStart());
+
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then((res) => {
+        dispatch(fetchSucess(res.data));
+      })
+      .catch((err) => {
+        dispatch(fetchFail("Gathering Failed"));
+      });
+  };
+};
+
+export const fetchStart = () => {
+  return { type: FETCH_START };
+};
+
+export const fetchSucess = (smurfs) => {
+  return { type: FETCH_SUCCESS, payload: smurfs };
+};
+
+export const fetchFail = (error) => {
+  return { type: FETCH_FAIL, payload: error };
+};
 
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
